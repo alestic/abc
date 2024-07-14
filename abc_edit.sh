066cbe45fd290539c1f1015229c0ee2b5ca7a2c1
Eric Hammond <ehammond@thinksome.com>
2024-07-13 19:10:21 -0700

    will explain how to use this magic later

---
+++ abc_edit.sh
@@ -0,0 +1,7 @@
+abc() {
+    local abc_cmd=$(command abc --mode norun "$@")
+    read -e -p "$(printf "%s" "${PS1@P}")" -i "$abc_cmd" user_cmd
+    history -s $(history 1 | sed 's/^ *[0-9]* *//')
+    history -s "$user_cmd"
+    eval "$user_cmd"
+}
